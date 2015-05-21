e80eca9135b533fa3038ff9c6eac57018a97a9f4
takymatica <ta.kymatica@gmail.com>
2015-05-21 15:32:30 -0600

    add second api call, check for parsed data

--- controllers.js
+++ controllers.js
@@ -3,11 +3,24 @@ var ngApp = angular.module('ng-app', []);
 ngApp.controller('repoCntrl', function($scope, $http) {
 
   $scope.repoData = function(){
+
     $http.get('https://bitbucket.org/api/2.0/repositories/battlemidget/python-salesforce/')
       .success(function(data){
-        $scope.data = data;
+        $scope.mainRepo = data;
+
       });
   }
 
+
+  $scope.commitsData = function(){
+  
+    $http.get('https://bitbucket.org/api/2.0/repositories/battlemidget/python-salesforce/commits')
+    .success(function(data){
+      $scope.commitsData = data;
+    });
+  }
+
   $scope.repoData();
+  $scope.commitsData();
+
 });
