7c07a4716b8da42d0f2f20d5d015cf91b15dfe91
takymatica <ta.kymatica@gmail.com>
2015-05-22 17:05:52 -0600

    hack together loop for each AIP page - (can't get .length to work to determine size of array [thanks Angular])

--- controllers.js
+++ controllers.js
@@ -2,13 +2,34 @@ var ngApp = angular.module('ng-app', []);
 
 ngApp.controller('repoCntrl', function($scope, $http) {
 
-  $scope.commitArray = [];
-  $scope.commitsData = function(){
+  $scope.commitArray1 = [];
+  $scope.commitsData1 = function(){
   
     $http.get('https://bitbucket.org/api/2.0/repositories/battlemidget/python-salesforce/commits')
       .success(function(data){
-        $scope.commitArray = data;
+        $scope.commitArray1 = data;
     });
   }
-  $scope.commitsData();
+  $scope.commitsData1();
+
+  $scope.commitArray2 = [];
+  $scope.commitsData2 = function(){
+  
+    $http.get('https://bitbucket.org/api/2.0/repositories/battlemidget/python-salesforce/commits?page=2')
+      .success(function(data){
+        $scope.commitArray2 = data;
+    });
+  }
+  $scope.commitsData2();
+
+  $scope.commitArray3 = [];
+  $scope.commitsData3 = function(){
+  
+    $http.get('https://bitbucket.org/api/2.0/repositories/battlemidget/python-salesforce/commits?page=3')
+      .success(function(data){
+        $scope.commitArray3 = data;
+    });
+  }
+  $scope.commitsData3();
+
 });
